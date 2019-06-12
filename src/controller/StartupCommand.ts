2f4d799eab7ba0b559d6205e10fb4e9b2662eb36
RollinSafary <bitbucket@planet221b.com>
2019-06-12 21:35:56 +0400

    minor changes

--- src/controller/StartupCommand.ts
+++ src/controller/StartupCommand.ts
@@ -1,14 +1,6 @@
 import { AsyncMacroCommand, SimpleCommand } from '@candywings/pure-mvc';
-import {
-  FB_INSTANT_COLLECTION_NAME,
-  OK_INSTANT_COLLECTION_NAME,
-  platform,
-} from '../constants/Constants';
-import GameFacade from '../GameFacade';
 import GameVOProxy from '../model/GameVOProxy';
 import PlayerVOProxy from '../model/PlayerVOProxy';
-import FBInstantWrapper from '../utils/FBInstantWrapper';
-import OKInstantWrapper from '../utils/OKInstantWrapper';
 import RegisterAdsCommands from './RegisterAdsCommands';
 import RegisterGameCommands from './RegisterGameCommands';
 import RegisterPlayerCommands from './RegisterPlayerCommands';
@@ -20,33 +12,7 @@ const forceData: IUserData = {
 
 export default class StartupCommand extends AsyncMacroCommand<SimpleCommand> {
   public async execute(notificationName: string): Promise<void> {
-    const url: string = window.location.href;
-    const okIndex: number = url.indexOf('ok');
-    const fbIndex: number = url.indexOf('facebook');
-    const fbInstantIndex: number = url.indexOf('fb');
-    const messangerIndex: number = url.indexOf('messanger');
-    if (okIndex !== -1) {
-      PlayerVOProxy.platform = platform.OK;
-    } else if (
-      fbIndex !== -1 ||
-      messangerIndex !== -1 ||
-      fbInstantIndex !== -1
-    ) {
-      PlayerVOProxy.platform = platform.FB;
-    }
-    let data: IUserData;
-    switch (PlayerVOProxy.platform) {
-      case platform.OK:
-        data = await this.initOK();
-        break;
-      case platform.FB:
-        data = await this.initFB();
-        break;
-      default:
-        PlayerVOProxy.platform = platform.MOBILE;
-        console.warn(`platform isn't instant`);
-    }
-    this.startProxyInitialization(data || forceData);
+    this.startProxyInitialization(forceData);
     super.execute(notificationName);
   }
 
@@ -54,29 +20,6 @@ export default class StartupCommand extends AsyncMacroCommand<SimpleCommand> {
     this.addSubCommand(RegisterGameCommands);
     this.addSubCommand(RegisterPlayerCommands);
     this.addSubCommand(RegisterAdsCommands);
-  }
-
-  private async initOK(): Promise<any> {
-    await OKInstantWrapper.initializeAsync();
-    this.sendNotification(GameFacade.OK_INSTANT_INITIALIZE_SUCCESS);
-    PlayerVOProxy.COLLECTION_NAME = OK_INSTANT_COLLECTION_NAME;
-    const data: IUserData = {
-      id: await OKInstantWrapper.playerGetID(),
-      name: await OKInstantWrapper.playerGetName(),
-    };
-    return data;
-  }
-  private async initFB(): Promise<any> {
-    await FBInstantWrapper.initializeAsync();
-    FBInstantWrapper.setLoadingProgress(100);
-    await FBInstantWrapper.startGameAsync();
-    PlayerVOProxy.COLLECTION_NAME = FB_INSTANT_COLLECTION_NAME;
-    this.sendNotification(GameFacade.FB_INSTANT_INITIALIZE_SUCCESS);
-    const data: IUserData = {
-      id: FBInstantWrapper.playerGetID(),
-      name: FBInstantWrapper.playerGetName(),
-    };
-    return data;
   }
 
   private startProxyInitialization(data: IUserData): void {
--- src/view/scenes/BootScene.ts
+++ src/view/scenes/BootScene.ts
@@ -1,5 +1,4 @@
 import { Images } from '../../assets';
-import { gameConfig } from '../../constants/GameConfig';
 import Game from '../../Game';
 import PlayerVOProxy from '../../model/PlayerVOProxy';
 import { loadImages } from '../utils/assetLoader';
@@ -13,7 +12,6 @@ export default class BootScene extends BaseScene {
   public static LOAD_COMPLETE_EVENT: string = `${BootScene.NAME}loadComplete`;
 
   public game: Game;
-  private logo: Phaser.GameObjects.Sprite;
   constructor() {
     super(BootScene.NAME);
   }
@@ -22,29 +20,15 @@ export default class BootScene extends BaseScene {
   }
 
   public create(): void {
-    this.logo = this.add.sprite(
-      gameConfig.canvasWidth / 2,
-      gameConfig.canvasHeight / 2,
-      Images.Logo.Name,
-    );
-    this.logo.alpha = 0;
-    this.tweens.add({
-      targets: this.logo,
-      alpha: 1,
-      duration: 1500,
-      yoyo: true,
-      onComplete: () => {
-        this.i18n.initialize(
-          {
-            fallbackLng: PlayerVOProxy.defaultLanguage,
-            loadPath: 'assets/locales/{{lng}}/{{ns}}.json',
-            debug: false,
-          },
-          () => {
-            this.events.emit(BootScene.LOAD_COMPLETE_EVENT);
-          },
-        );
+    this.i18n.initialize(
+      {
+        fallbackLng: PlayerVOProxy.defaultLanguage,
+        loadPath: 'assets/locales/{{lng}}/{{ns}}.json',
+        debug: false,
       },
-    });
+      () => {
+        this.events.emit(BootScene.LOAD_COMPLETE_EVENT);
+      },
+    );
   }
 }
